a191fe143f6aac8aa4678fcc74ba8def596ab178
Gabriel Rodriguezu Ubiera <grodriguezubiera@gmail.com>
2020-06-21 15:56:51 -0400

    Masora v1.0.2

--- masora.js
+++ masora.js
@@ -6,27 +6,27 @@ const mails = require('./utils/mails');
 
 const randomNumber = (length) => {
     let randomNumber = 0;
-    if(length === undefined){
+    if (length === undefined) {
         console.log('%cMasora: (Random Numbers): You must put a limit as a parameter.', 'color: orange');
-    } else if(length === 0){
+    } else if (length === 0) {
         randomNumber = Math.floor(Math.random() * 0);
         console.log('%cMasora: (Random Numbers): If you use 0 as a limit, a random number is not generated.', 'color: orange');
-    } else if(length > 0 && length <= 16){
+    } else if (length > 0 && length <= 16) {
         let digits = "1";
-        for(let i = 0; i<length; i++){
+        for (let i = 0; i < length; i++) {
             digits += "0";
         }
         digits = parseInt(digits);
-        for(let i = 0; i<10000; i++){
-            if(randomNumber === 0){
+        for (let i = 0; i < 10000; i++) {
+            if (randomNumber === 0) {
                 randomNumber = Math.floor(Math.random() * 10);
-            } else if(randomNumber.toString().length === length){
+            } else if (randomNumber.toString().length === length) {
                 break;
             } else {
                 randomNumber = Math.floor(Math.random() * digits);
             }
-        }   
-    } else if(length > 16){
+        }
+    } else if (length > 16) {
         console.log('%cMasora: (Random Numbers): You passed the limit of digits, use less than 16 digits.', 'color: orange');;
     }
     return randomNumber;
@@ -34,9 +34,9 @@ const randomNumber = (length) => {
 
 // return an number from your digit paramss
 
-function returnNumberDigits(digits, limit){
+function returnNumberDigits(digits, limit) {
     let i = randomNumber(digits);
-    while(i > limit){
+    while (i > limit) {
         i = randomNumber(digits);
     }
     return i;
@@ -44,14 +44,14 @@ function returnNumberDigits(digits, limit){
 
 // Random names
 
-const randomName = () => {    
-    let i  = returnNumberDigits(2, 15);
-    let name = names[i];
-    return name;
-}
-// Random lastnames
+const randomName = () => {
+        let i = returnNumberDigits(2, 15);
+        let name = names[i];
+        return name;
+    }
+    // Random lastnames
 
-const randomLastname = () => {    
+const randomLastname = () => {
     let i = returnNumberDigits(3, 450);
     let lastname = lastnames[i];
     return lastname;
@@ -62,12 +62,12 @@ const randomLastname = () => {
 const randomMail = () => {
     let name = randomName();
     let lastname = randomLastname();
-    let i = returnNumberDigits(1,3);
+    let i = returnNumberDigits(1, 3);
     let mail;
-    if(name.length >= 6){
+    if (name.length >= 6) {
         mail = `${name}${randomNumber(2)}${mails[i]}`;
     } else {
-        mail = `${name}${lastname}${mails[i]}`;        
+        mail = `${name}${lastname}${mails[i]}`;
     }
     return mail.toLocaleLowerCase();
 }
@@ -77,18 +77,14 @@ const randomMail = () => {
 const randomUsername = () => {
     let name = randomName();
     let lastname = randomLastname();
-    let i = returnNumberDigits(1,3);
+    let i = returnNumberDigits(1, 3);
     let username;
-    if(name.length >= 6){
+    if (name.length >= 6) {
         username = `${name}${randomNumber(2)}`;
     } else {
-        username = `${name}${lastname}`;        
+        username = `${name}${lastname}`;
     }
     return username.toLocaleLowerCase();
-}
-
-const randomDate = (s) => {
-   
 }
 
 module.exports = {
---
+++ test.js
@@ -0,0 +1,22 @@
+const masora = require('./masora');
+
+console.log({
+    "FunctionName": "RandomNumbers",
+    "RandomNumber(3)": masora.randomNumber(3)
+});
+console.log({
+    "FunctionName": "RandomName",
+    "RandomName()": masora.randomName()
+});
+console.log({
+    "FunctionName": "RandomLastname",
+    "RandomLastname()": masora.randomLastname()
+});
+console.log({
+    "FunctionName": "RandomUsername",
+    "RandomUsername()": masora.randomUsername()
+});
+console.log({
+    "FunctionName": "RandomMail",
+    "RandomMail()": masora.randomMail()
+});
